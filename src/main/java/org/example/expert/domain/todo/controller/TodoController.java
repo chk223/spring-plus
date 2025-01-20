d5e22f7a2a06f985b1ba1aed1ce9abd987fb5eb5
chk223 <kch23_71@naver.com>
2025-01-20 13:59:00 +0900

    Lv3-10: QueryDSL 을 사용하여 검색 기능 구현 - todo/search

--- src/main/java/org/example/expert/domain/todo/controller/TodoController.java
+++ src/main/java/org/example/expert/domain/todo/controller/TodoController.java
@@ -7,6 +7,7 @@ import org.example.expert.domain.common.dto.AuthUser;
 import org.example.expert.domain.todo.dto.request.TodoSaveRequest;
 import org.example.expert.domain.todo.dto.response.TodoResponse;
 import org.example.expert.domain.todo.dto.response.TodoSaveResponse;
+import org.example.expert.domain.todo.dto.response.TodoSearchResponse;
 import org.example.expert.domain.todo.service.TodoService;
 import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
@@ -42,5 +43,17 @@ public class TodoController {
     @GetMapping("/todos/{todoId}")
     public ResponseEntity<TodoResponse> getTodo(@PathVariable long todoId) {
         return ResponseEntity.ok(todoService.getTodo(todoId));
+    }
+
+    @GetMapping("/todos/search")
+    public ResponseEntity<Page<TodoSearchResponse>> searchTodo(
+            @RequestParam(defaultValue = "1") int page,
+            @RequestParam(defaultValue = "10") int size,
+            @RequestParam(required = false) String searchKeyword,
+            @RequestParam(required = false) LocalDateTime startDate,
+            @RequestParam(required = false) LocalDateTime endDate,
+            @RequestParam(required = false) String managerNickname
+    ) {
+        return ResponseEntity.ok(todoService.searchTodo(page, size, searchKeyword,startDate, endDate,managerNickname));
     }
 }
--- src/main/java/org/example/expert/domain/todo/repository/TodoRepositoryImpl.java
+++ src/main/java/org/example/expert/domain/todo/repository/TodoRepositoryImpl.java
@@ -1,10 +1,20 @@
 package org.example.expert.domain.todo.repository;
 
+import com.querydsl.core.BooleanBuilder;
+import com.querydsl.core.types.Projections;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import lombok.RequiredArgsConstructor;
+import org.example.expert.domain.manager.entity.QManager;
+import org.example.expert.domain.todo.dto.response.TodoSearchResponse;
 import org.example.expert.domain.todo.entity.QTodo;
 import org.example.expert.domain.todo.entity.Todo;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.Pageable;
+import org.springframework.util.StringUtils;
 
+import java.time.LocalDateTime;
+import java.util.List;
 import java.util.Optional;
 
 @RequiredArgsConstructor
@@ -20,5 +30,64 @@ public class TodoRepositoryImpl implements TodoRepositoryCustom {
                 .fetchJoin()
                 .where(todo.id.eq(todoId))
                 .fetchOne());
+    }
+
+    @Override
+    public Page<TodoSearchResponse> findBySearch(String searchKeyword, String managerNickname, LocalDateTime startDate, LocalDateTime endDate, Pageable pageable) {
+        QTodo todo = QTodo.todo;
+        QManager manager = QManager.manager;
+        BooleanBuilder builder = getConditionsForSearch(searchKeyword, managerNickname, startDate, endDate, todo);
+
+        List<TodoSearchResponse> content = getContent(pageable, todo, manager, builder);
+
+        Long totalCount = getTotalCount(todo, builder);
+
+        long total = totalCount == null ? 0L : totalCount;
+
+        return new PageImpl<>(content, pageable, total);
+    }
+
+    private List<TodoSearchResponse> getContent(Pageable pageable, QTodo todo, QManager manager, BooleanBuilder builder) {
+        return queryFactory
+                .select(Projections.constructor(TodoSearchResponse.class,
+                        todo.title,
+                        todo.managers.size(),
+                        todo.comments.size()))
+                .from(todo)
+                .leftJoin(todo.managers, manager)
+                .where(builder)
+                .orderBy(todo.createdAt.desc())
+                .offset(pageable.getOffset())
+                .limit(pageable.getPageSize())
+                .fetch();
+    }
+
+    private Long getTotalCount(QTodo todo, BooleanBuilder builder) {
+        return queryFactory
+                .select(todo.count())
+                .from(todo)
+                .where(builder)
+                .fetchOne();
+    }
+
+    private static BooleanBuilder getConditionsForSearch(String searchKeyword, String managerNickname, LocalDateTime startDate, LocalDateTime endDate, QTodo todo) {
+        BooleanBuilder builder = new BooleanBuilder();
+
+        if (StringUtils.hasText(searchKeyword)) {
+            builder.and(todo.title.contains(searchKeyword));
+        }
+
+        if (startDate != null) {
+            builder.and(todo.createdAt.goe(startDate));
+        }
+
+        if (endDate != null) {
+            builder.and(todo.createdAt.loe(endDate));
+        }
+
+        if (StringUtils.hasText(managerNickname)) {
+            builder.and(todo.managers.any().user.nickname.contains(managerNickname));
+        }
+        return builder;
     }
 }
